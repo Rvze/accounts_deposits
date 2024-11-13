c931cb4b42b5d173cc267a71eecaf5db8ba2e30f
drslkv <drslkv@MacBook-Air-Dmitrij-2.local>
2024-11-13 16:11:04 +0300

    Объединение проекта

---
+++ src/main/java/drslkv/code/accounts_deposits/AccountsDepositsApplication.java
@@ -0,0 +1,13 @@
+package drslkv.code.accounts_deposits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsDepositsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsDepositsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/drslkv/code/accounts_deposits/AccountsDepositsApplicationTests.java
@@ -0,0 +1,13 @@
+package drslkv.code.accounts_deposits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsDepositsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
